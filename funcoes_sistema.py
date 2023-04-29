658f3a09c09b507dee71e43a9a6163fa11b9b94b
Lucas Polli <lucaspolli3@gmail.com>
2023-04-28 23:33:14 -0300

    Registro do usuário implementado e funcionando

--- funcoes_sistema.py
+++ funcoes_sistema.py
@@ -4,42 +4,25 @@ import sqlite3
 connection = sqlite3.connect('banco_de_dados.db')
 cursor = connection.cursor()
 
-cart = []
-# PRODUTOS
-products = [{"id": 55, "name": 'Teclado Logitech', "price": 359.99, "amount": 1},
-            {"id": 256, "name": 'Fone sem fio', "price": 150.00, "amount": 1},
-            {"id": 541, "name": 'Iphone 16', "price": 20000.00, "amount": 1},
-            {"id": 13, "name": 'Nokia', "price": 135.50, "amount": 1},
-            {"id": 45, "name": 'Monitor 19"', "price": 700.00, "amount": 1},
-            {"id": 698, "name": 'TV Samsung 4k 75"', "price": 5700.00, "amount": 1},
-            {"id": 1243, "name": 'PC Gamer Gtx 1050Ti', "price": 5000.00, "amount": 1},
-            {"id": 908, "name": 'Mouse Razzer', "price": 99.99, "amount": 1},
-            {"id": 258, "name": 'Tablet CCE', "price": 450.00, "amount": 1},
-            {"id": 157, "name": 'Copo stanley', "price": 300.00, "amount": 1}]
 # INÍCIO
-ecommerce = "E - C O M M E R C E"
-do = "D O"
-lukako = "L U K A K O"
+sistema = "S I S T E M A"
+comercial = "C O M E R C I A L"
 
 def inicio():
     print("="*57)
-    print(f"{ecommerce:^57}")
-    print(f"{do:^57}")
-    print(f"{lukako:^57}")
+    print(f"{sistema:^57}")
+    print(f"{comercial:^57}")
     print("="*57)
 # MENU
 def menu():
-    sleep(0.2)
     print("="*57)
-    sleep(0.2)
-    print("1 - Adicionar um produto ao carrinho")
-    sleep(0.2)
-    print("2 - Remover um produto do carrinho")
-    sleep(0.2)
-    print("3 - Consultar valor do carrinho")
-    sleep(0.2)
-    print("4 - Sair do menu")
-    sleep(0.2)
+    sleep(0.7)
+    print("1 - Registrar")
+    sleep(0.4)
+    print("2 - Logar")
+    sleep(0.4)
+    print("3 - Sair do menu")
+    sleep(0.4)
     while True:
         try:
             opcao = int(input("\033[0;33mEscolha uma das opções acima para continuar: \033[m"))
@@ -48,75 +31,64 @@ def menu():
             sleep(1)
             print(f"\033[0;31mDigite um número!\033[m")
     return opcao
-# ADICIONAR UM PRODUTO
-def adicionar_produto():
+# REGISTRAR CONTA =================================================================================================
+def Register():
     print("="*57)
-    sleep(1)
-    print("\033[0;32mSegue abaixo uma lista dos nosso produtos disponíveis no estoque:\033[m")
-    sleep(1)
-    print("\033[0;33mNome                    Preço          Id\033[m")
-    for produto in products:
-        name = produto.get("name")
-        price = produto.get("price")
-        id = produto.get("id")
-        sleep(0.2)
-        print(f"{name:<23}", f"{price:<12.2f}", "(", f"{id}", ")")
-    sleep(0.2)
-    print("="*57)
-    return_to_the_question = True
-    while return_to_the_question == True:
-        while True:
-            try:
-                sleep(1)
-                product = int(input("\033[0;33mDigite o ID do produto que você deseja adicionar ao carrinho: \033[m"))
-                break
-            except ValueError:
-                sleep(1)
-                print(f"\033[0;31mDigite um número!\033[m")
-        sleep(0.5)
-        added = False
-        for produto in products:
-            name = produto.get("name")
-            ids = produto.get("id")
-            price = produto.get("price")
-            amount = produto.get("amount")
-            if product == ids:
-                sleep(1)
-                cursor.execute(f"SELECT * FROM dados WHERE id = {product}")
-                resultado = cursor.fetchall()
-                if resultado == []:
-                    pago = 'nao'
-                    comando = f"INSERT INTO dados (nome, id, preço, quantidade, pago) VALUES('{name}', '{product}', '{price}', '{amount}', '{pago}')"
-                    cursor.execute(comando)
-                    print(f"\033[0;32m{name} adicionado ao carrinho!\033[m")
-                    added = True
-                    connection.commit()
-                    break
-                else:
-                    cursor.execute(f"SELECT quantidade FROM dados WHERE id = {product}")
-                    quantidade = cursor.fetchall()
-                    quantidade2 = int(quantidade[0][0])
-                    quantidade2 += 1
-                    comando2 = (f'UPDATE dados SET quantidade = {quantidade2} WHERE ID = {product}') ######
-                    cursor.execute(comando2)
-                    connection.commit()
-                    print(f"\033[0;32mMais um(a) {name} adicionado ao carrinho!\033[m")
-                    added = True
-                    break
-            elif added == False and ids == 157:
-                sleep(1)
-                print(f"\033[0;31mProduto não encontrado!\033[m")
-        sleep(0.5)
-        while True:
-            escolha = str(input("\033[0;33mDeseja adicionar mais itens ao carrinho? (sim/nao) \033[m"))
-            if escolha in "nao":
-                return_to_the_question = False
-                break
-            elif escolha in "sim":
-                break
-            else:
-                sleep(1)
-                print(f"\033[0;31mDigite apenas: sim/nao\033[m")
+    # CONFIRMAÇÃO DO NOME DO USUÁRIO
+    username_confirmation = False
+    while username_confirmation == False:
+        sleep(1)
+        username = str(input("\033[0;33mDigite seu nome de usuário: \033[m"))
+        cursor.execute(f"SELECT * FROM clientes WHERE usuario = '{username}'")
+        resultado = cursor.fetchall()
+        if resultado != []:
+            sleep(1.2)
+            print("\033[0;31mEsse usuário já existe!\033[m")
+            username_confirmation = False
+        else:
+            username_confirmation = True
+    # CONFIRMAÇÃO DO EMAIL
+    email_confirmation = False
+    while email_confirmation == False:
+        sleep(1)
+        email = str(input("\033[0;33mDigite seu email: \033[m"))
+        cursor.execute(f"SELECT * FROM clientes WHERE email = '{email}'")
+        resultado = cursor.fetchall()
+        if resultado != []:
+            sleep(1.2)
+            print("\033[0;31mEsse email já foi cadastrado!\033[m")
+            email_confirmation = False
+        else:
+            email_confirmation = True
+    # CONFIRMAÇÃO DA SENHA
+    password_confirmation = False
+    while password_confirmation == False:
+        sleep(1)
+        password = str(input("\033[0;33mDigite sua senha: \033[m"))
+        sleep(1)
+        password_confirm = str(input("\033[0;33mConfirme a sua senha: \033[m"))
+        cursor.execute("SELECT * from clientes")
+        resultado = cursor.fetchall()
+        if resultado == []:
+            id = 0
+        else:
+            id = 0
+            for users in resultado:
+                id = id + 1
+        if password != password_confirm:
+            sleep(1.2)
+            print("\033[0;31mAs senhas não se coincidem!\033[m")
+            password_confirmation = False
+        else:
+            comand = f"INSERT INTO clientes (usuario, email, senha, id) VALUES('{username}', '{email}', '{password}', '{id}')"
+            cursor.execute(comand)
+            connection.commit()
+            sleep(1)
+            print("\033[0;32mSeu usuário foi adicionado ao sistema!\033[m")
+            password_confirmation = True
+            sleep(0.5)
+            logged = True
+    return logged
 # REMOVER UM PRODUTO
 def remover_produto():
     print("="*57)
--- sistema.py
+++ sistema.py
@@ -6,30 +6,31 @@ connection = sqlite3.connect('banco_de_dados.db')
 cursor = connection.cursor()
 
 def create_table():
-    cursor.execute('CREATE TABLE IF NOT EXISTS dados (nome varchar(20), id varchar(4), preço varchar(8), quantidade varchar(20), pago text)')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS clientes (
+                        usuario varchar(20),
+                        email varchar(20), 
+                        senha varchar(20), 
+                        id varchar(4))''')
+    #preço varchar(8), quantidade varchar(20), pago text
 
 create_table()
 
 inicio()
-sleep(1)
-print("\033[0;33mSeja bem vindo(a) ao maior e-commerce da América Latina!\033[m")
-sleep(1.5)
+sleep(0.5)
+print("\033[0;33mSeja bem vindo(a) ao maior sistema comercial da América Latina!\033[m")
 key = True
 valor_pago = 0
-while key == True:
+logged = False
+while logged == False:
     sleep(0.5)
     opcao = menu()
     if opcao == 1:
-        sleep(1)
-        adicionar_produto()
+        sleep(0.5)
+        logged = Register()
     elif opcao == 2:
         sleep(1)
         remover_produto()
     elif opcao == 3:
-        sleep(1)
-        valor_pago = consultar_carrinho(valor_pago)
-    elif opcao == 4:
-        sleep(1)
         connection.close()
         break
     else:
@@ -38,4 +39,4 @@ while key == True:
 sleep(1)
 print("="*57)
 sleep(0.5)
-print("\033[0;33mObrigado por usar o nosso e-commerce! Volte sempre!\033[m")
+print("\033[0;32mObrigado por usar o nosso sistema! Volte sempre!\033[m")
